fd96875bcd2d1bd5e172f030d9df3e84ef01931f
Jeeho Ahn <io@jcde.xyz>
2023-11-20 16:24:17 +0900

    asdf

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,44 @@ export default function Home() {
           established brand, I can create a web experience that connects with
           your audience and helps your business grow.
         </p>
+        <div className="py-16">
+          <h1 className="text-4xl appear">Experience</h1>
+          <ul className="space-y-3 mt-6 text-gray-11">
+            <li className="appear">
+              <a href="https://dankook.sen.hs.kr">
+                Dankook University Affiliated Software Highschool
+              </a>{" "}
+              - Student
+            </li>
 
+            <li className="appear">
+              <a href="https://devbench.kr">DevBench</a> - Web Designer &
+              Moderator
+            </li>
+            <li>
+              <a href="https://vignetteapp.org">Vignette</a> - Maintainer &
+              Community Moderator
+            </li>
+            <li className="appear">
+              <a href="http://gifted.snu.ac.kr/">
+                SNU Science Education Institute for Gifted
+              </a>
+              {` `}- Graduate
+            </li>
+            <li className="appear">
+              <a href="https://talented.snue.ac.kr/">
+                SNUE Software Gifted Education Center
+              </a>{" "}
+              - Graduate
+            </li>
+            <li className="appear">
+              2019 Samsung Junior Software Cup - Finalist
+            </li>
+            <li className="appear">
+              2022 ICRES Student Discussion Competition - Bronze Medalist
+            </li>
+          </ul>
+        </div>
         <Stack />
         <div className=" mt-12 flex flex-col space-y-6 border rounded-xl p-6 py-8 bg-gray-2">
           <div className="font-semibold text-3xl">
