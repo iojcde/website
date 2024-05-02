bb7a2dda778ec98e75732d803dfd8a07dbc079bd
Jeeho Ahn <io@jcde.xyz>
2024-05-03 06:47:42 +0900

    teto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx'; 
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
@@ -8,7 +7,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind({applyBaseStyles:false}), react()], devToolbar: {
+  integrations: [mdx(),   tailwind({applyBaseStyles:false}), react()], devToolbar: {
     enabled: false
   }
 });
